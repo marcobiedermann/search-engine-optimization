2464637a618e77bbf83c63f5a8c5da7dc33ba653
Marco Biedermann <marco.biedermann@gmx.de>
2019-07-05 16:38:43 +0200

    fix: Linting
    fix linting issues

--- js/script.js
+++ js/script.js
@@ -18,19 +18,45 @@
 
   // easing
   var easing = {
-    linear: function (t) { return t; },
-    easeInQuad: function (t) { return t * t; },
-    easeOutQuad: function (t) { return t * (2 - t); },
-    easeInOutQuad: function (t) { return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t; },
-    easeInCubic: function (t) { return t * t * t; },
-    easeOutCubic: function (t) { return (--t) * t * t + 1; },
-    easeInOutCubic: function (t) { return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1; },
-    easeInQuart: function (t) { return t * t * t * t; },
-    easeOutQuart: function (t) { return 1 - (--t) * t * t * t; },
-    easeInOutQuart: function (t) { return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t; },
-    easeInQuint: function (t) { return t * t * t * t * t; },
-    easeOutQuint: function (t) { return 1 + (--t) * t * t * t * t; },
-    easeInOutQuint: function (t) { return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t; }
+    linear: function(t) {
+      return t;
+    },
+    easeInQuad: function(t) {
+      return t * t;
+    },
+    easeOutQuad: function(t) {
+      return t * (2 - t);
+    },
+    easeInOutQuad: function(t) {
+      return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+    },
+    easeInCubic: function(t) {
+      return t * t * t;
+    },
+    easeOutCubic: function(t) {
+      return --t * t * t + 1;
+    },
+    easeInOutCubic: function(t) {
+      return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+    },
+    easeInQuart: function(t) {
+      return t * t * t * t;
+    },
+    easeOutQuart: function(t) {
+      return 1 - --t * t * t * t;
+    },
+    easeInOutQuart: function(t) {
+      return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t;
+    },
+    easeInQuint: function(t) {
+      return t * t * t * t * t;
+    },
+    easeOutQuint: function(t) {
+      return 1 + --t * t * t * t * t;
+    },
+    easeInOutQuint: function(t) {
+      return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t;
+    },
   };
 
   // requestAnimationFrame
@@ -76,10 +102,10 @@
 
     function scroll(timestamp) {
       var currentTime = Date.now();
-      var time = Math.min(1, ((currentTime - start) / duration));
+      var time = Math.min(1, (currentTime - start) / duration);
       var easedT = easingFunction(time);
 
-      elem.scrollTop = (easedT * (Y - from)) + from;
+      elem.scrollTop = easedT * (Y - from) + from;
 
       if (time < 1) {
         requestAnimationFrame(scroll);
@@ -87,9 +113,7 @@
         if (callback) {
           callback();
         }
-
       }
-
     }
 
     requestAnimationFrame(scroll);
@@ -104,9 +128,8 @@
   length = links.length;
 
   for (i = 0; i < length; i++) {
-     if (links[i].hostname != window.location.hostname) {
-         links[i].target = '_blank';
-     }
+    if (links[i].hostname != window.location.hostname) {
+      links[i].target = '_blank';
+    }
   }
-
-}(window, document));
+})(window, document);
